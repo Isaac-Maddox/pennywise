0e6894d6e4efb62ecdd24ed23fabc9d46374bccb
isaacmaddox <isaacmaddox05@gmail.com>
2024-11-21 09:29:05 -0600

    ...

--- src/app/(pages)/(auth)/logout/route.ts
+++ src/app/(pages)/(auth)/logout/route.ts
@@ -1,9 +1,16 @@
+import { getUserbyToken } from "@/middleware_funcs";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function GET() {
    const cookieStore = await cookies();
+   const token = cookieStore.get("usrjwt")?.value;
+
+   const user = await getUserbyToken(token);
+
+   console.log("Logging out", user);
+
    cookieStore.delete("usrjwt");
 
-   redirect('/login');
+   redirect("/login");
 }
